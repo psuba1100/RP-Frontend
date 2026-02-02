30bc8649772d9b581eafed78454af2bb01d3fd0d
psuba1100 <92215854+psuba1100@users.noreply.github.com>
2026-02-02 22:03:14 +0100

    Audited packages, added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Analytics } from "@vercel/analytics/react"
 import 'katex/dist/katex.min.css';
 
 import './styles/css/root.css'
@@ -13,9 +14,10 @@ import './styles/css/card.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+      <Analytics />
       <Routes>
-        <Route path='/*' element={<App />} />
+        <Route path="/*" element={<App />} />
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )
